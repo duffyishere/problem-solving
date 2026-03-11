57941aadd7f1bececf8fc55628dda87a4358370d
zzunho9 <jamlove4763@naver.com>
2026-03-11 18:51:59 +0900

    solve: 보물(no.1026) 문제풀이 추가

---
+++ python/pr1026.py
@@ -0,0 +1,12 @@
+import sys
+
+
+input = sys.stdin.readline
+n = int(input().rstrip())
+a = sorted(list(map(int, input().split())))
+b = sorted(list(map(int, input().split())), reverse=True)
+
+result = 0
+for i in range(n):
+    result += a[i] * b[i]
+print(result)

63e49f6cd9470326d45da931b206dababbfead5b
zzunho9 <jamlove4763@naver.com>
2026-03-12 17:22:39 +0900

    solve: 좌표 압축(no.18870) 문제풀이 추가

---
+++ python/pr18870.py
@@ -0,0 +1,28 @@
+import sys
+
+
+input = sys.stdin.readline
+n = int(input().rstrip())
+nums = list(map(int, input().split()))
+sorted_nums = sorted(list(set(nums)))
+
+def find_left_idx(target: int, nums: list):
+    start = 0
+    end = len(nums)
+
+    while start < end:
+        mid = int((start + end) / 2)
+        if nums[mid] >= target:
+            end = mid 
+        else:
+            start = mid + 1
+    return start
+
+idx_dict = {}
+result = []
+for n in nums:
+    if n not in idx_dict:
+        idx_dict[n] = find_left_idx(n, sorted_nums)
+    result.append(idx_dict[n])
+
+print(*result)

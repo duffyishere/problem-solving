5b151877dd5f5f860b6f226c6f56e1870a29ab9b
zzunho9 <jamlove4763@naver.com>
2026-04-01 14:01:24 +0900

    solve: 절댓값 힙(no.11286) 문제풀이 추가

---
+++ python/pr11286.py
@@ -0,0 +1,26 @@
+from collections import Counter
+import heapq
+import sys
+
+
+input = sys.stdin.readline
+n = int(input())
+a = [int(input()) for _ in range(n)]
+heap = []
+count = {}
+
+for num in a:
+    count[num] = count.get(num, 0) + 1
+    if num == 0:
+        if heap:
+            min_num = heapq.heappop(heap)
+            if 0 < count.get(-min_num, 0):
+                print(-min_num)
+                count[-min_num] = count.get(-min_num, 0) - 1
+            else:
+                print(min_num)
+                count[min_num] = count.get(min_num, 0) - 1
+        else:
+            print(0)
+    else:
+        heapq.heappush(heap, abs(num))

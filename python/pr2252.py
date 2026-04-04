17c200f6a1939a39822d2d2e91a9af28864681c5
zzunho9 <jamlove4763@naver.com>
2026-04-04 17:48:13 +0900

    solve: 줄 세우기(no.2252) 문제풀이 추가

---
+++ python/pr2252.py
@@ -0,0 +1,29 @@
+from collections import deque
+import sys
+
+
+input = sys.stdin.readline
+n, m = map(int, input().split())
+a = [[] for _ in range(n + 1)]
+indegree_cnt = [0 for _ in range(n + 1)]
+indegree_cnt[0] = -1
+for _ in range(m):
+    p1, p2 = map(int, input().split())
+    a[p1].append(p2)
+    indegree_cnt[p2] += 1
+
+q = deque()
+for idx, val in enumerate(indegree_cnt):
+    if val == 0:
+        q.append(idx)
+
+result = []
+while q:
+    curr = q.popleft()
+    result.append(curr)
+    for nxt in a[curr]:
+        indegree_cnt[nxt] -= 1
+        if indegree_cnt[nxt] == 0:
+            q.append(nxt)
+
+print(*result)

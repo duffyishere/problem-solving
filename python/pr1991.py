0f6078741ee847d81277fa22b525e07b389a6dec
zzunho9 <jamlove4763@naver.com>
2026-04-04 15:53:58 +0900

    solve: 트리 순회(no.1991) 문제풀이 추가

---
+++ python/pr1991.py
@@ -0,0 +1,36 @@
+import sys
+
+
+input = sys.stdin.readline
+n = int(input())
+a = [[] for _ in range(26)]
+for _ in range(n):
+    parent, left, right = map(lambda n1: ord(n1) - 65, input().split())
+    a[parent].append(left)
+    a[parent].append(right)
+
+result = [[] for _ in range(3)]
+def preorder(curr, a):
+    if curr < 0: return
+    result[0].append(chr(curr + 65))
+    preorder(a[curr][0], a)
+    preorder(a[curr][1], a)
+
+def inorder(curr, a):
+    if curr < 0: return
+    inorder(a[curr][0], a)
+    result[1].append(chr(curr + 65))
+    inorder(a[curr][1], a)
+
+def postorder(curr, a):
+    if curr < 0: return
+    postorder(a[curr][0], a)
+    postorder(a[curr][1], a)
+    result[2].append(chr(curr + 65))
+
+preorder(0, a)
+inorder(0, a)
+postorder(0, a)
+
+for r in result:
+    print("".join(r))
